ec9f8f80ec4dee2e473ad9b2558389ef3b17c5cf
Bisnisokteto <110271297+Bisnisokteto@users.noreply.github.com>
2022-08-22 21:02:47 +0700

    DikaaV2
    hhi

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -13,7 +13,7 @@ Berapapun donasi kalian akan sangat berarti 👍
 𝐀𝐫𝐢𝐠𝐚𝐭𝐨𝐮!!!
 
 Contact person Owner:
-wa.me/6285828357727 (Owner)`
+wa.me/6288223674569 (Owner)`
 let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/696076406bf2516adb1fd.jpg' }}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
@@ -24,14 +24,14 @@ let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/fil
            hydratedButtons: [{
              urlButton: {
                displayText: '🏧 Sociabuzz',
-               url: 'https://sociabuzz.com/arietube/tribe'
+               url: 'https://sociabuzz.com/DikaaOfc/Fanxxz'
              }
 
            },
                {
              callButton: {
                displayText: 'Gopay',
-               phoneNumber: '+62 882-4500-9287'
+               phoneNumber: '+62 882-2367-4569'
              }
            },           
                {
